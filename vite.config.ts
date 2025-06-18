ed4866faf1f7c39187a35ad0b313faa12045148c
AKDesigne <webalekskubanov@gmail.com>
2025-06-18 13:39:02 +0200

    fix: Remove unnecessary imports and aliases from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +9,6 @@ export default defineConfig({
   },
   publicDir: 'public',
   base: '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@public': resolve(__dirname, './public')
-    }
-  },
   server: {
     watch: {
       usePolling: true
